864d77d5eafd4a6ca9db8f5945694f3e8c8f062f
Luis Zaninetti <35489790+luchozaninetti@users.noreply.github.com>
2024-05-16 10:14:56 +0100

    Add files via upload

---
+++ Exercício 3.mk
@@ -0,0 +1,16 @@
+A) Criei utilizadores (Master e Slave)
+![](MasterSlave.PNG)
+
+B) Defini o número mínimo de 6 caracteres na pass, passwd
+
+C) Defini diretorios iniciais para cada utilizador o Master e o Slave
+
+D) Modifiquei o UID dos utilizadores
+![](UID.PNG)
+
+E) Criei um grupo (grupo1) e adicionei o utilizador Slave
+![](Grupo.PNG)
+
+F) 
+
+G)
