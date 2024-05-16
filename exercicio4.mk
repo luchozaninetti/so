9201b1a93ea5f34d733de572ba170938de70511c
Luis Zaninetti <35489790+luchozaninetti@users.noreply.github.com>
2024-05-16 11:07:22 +0100

    Add files via upload

---
+++ exercicio4.mk
@@ -0,0 +1,5 @@
+1)para instalar o servidor ubuntu usei o comando "sudo apt-get install cockpit -y"
+
+2)instalei todas as atualizações necessarias
+
+3) criei um novo utilizador e exclui o utilizador do ex anterior
