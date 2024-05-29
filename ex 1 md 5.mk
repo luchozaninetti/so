f64fc6efc883b0d901f6271d48a16ee38d416f97
Luis Zaninetti <35489790+luchozaninetti@users.noreply.github.com>
2024-05-29 14:29:39 +0100

    Add files via upload

---
+++ ex 1 md 5.mk
@@ -0,0 +1,11 @@
+A)Criei um ficheiro script bash usando o serviço samba
+
+B) Utilizando o comando wish bash para fazer o caminho ate o bash shell
+
+C)Fiz com q o ficheiro imprimisse a mensagem "hello world"
+
+D)Executei o script
+
+E)Alterei as funções como foi pedido
+
+F)Executei o script
---
+++ ex 2 md 5.mk
@@ -0,0 +1,5 @@
+A) O programa solicita ao usuario dois numeros e os le os mesmos
+
+B)Aparece as escolhas para realizar a operação que for escolhida
+
+C)Imprimi o resultado que foi pedido
